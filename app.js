b33a5e5fcb975a0b65bf56e9d5b0986d982d4a6b
aarohinadkarni <arunadkarni@gmail.com>
2023-11-30 13:16:37 -0500

    a6 first commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js"
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
@@ -9,6 +12,7 @@ import "dotenv/config";
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 Lab5(app)
 Hello(app)
