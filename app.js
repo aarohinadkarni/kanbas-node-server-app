904ee32c30d08aecbc7539138b49933e039bfcb1
aarohinadkarni <arunadkarni@gmail.com>
2023-12-03 14:48:50 -0500

    confiugre atlas

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ import Lab5 from "./Lab5.js"
 import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect("mongodb+srv://webdevsp:webDev2023@cluster0.r7yo1za.mongodb.net/?retryWrites=true&w=majority");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://a5--grand-entremet-075770.netlify.app"
+  })
+);   
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
